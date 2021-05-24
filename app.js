522406bb221246add76b0b129c5889d0e1bd87a2
Th-Svn <80069407+Th-Svn@users.noreply.github.com>
2021-05-23 23:30:51 -0500

    Commit manually
    The commits do not appear in my GitHub repository. The local repository is linked to this remote repository

---
+++ app.js
@@ -0,0 +1,9 @@
+// Declaring the dependencies and variables
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const writeFileAsync = util.promisify(fs.writeFile);
+//Prompt the user questions to populate the README.md
+// Async function using util.promisify 
+
+
